b2a566f650510e5278f0cc242fa27e51dd329f11
김종진 <martia0118@gmail.com>
2021-03-27 12:15:44 +0900

    프레그먼트 xml + 코틀린 코드 기초 작성(연결작업)

---
+++ app/src/main/java/com/nepplus/fragmentpractice_20210327/fragments/FirstFragment.kt
@@ -0,0 +1,21 @@
+package com.nepplus.fragmentpractice_20210327.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nepplus.fragmentpractice_20210327.R
+
+class FirstFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_first, container, false)
+
+    }
+}
