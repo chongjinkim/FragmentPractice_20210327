5cb1a7c400e74258ba8dab273501f655a8e153f6
김종진 <martia0118@gmail.com>
2021-03-27 12:20:08 +0900

    로그버튼 이벤트 추가 (Fragment의 이벤트처리)

--- app/src/main/java/com/nepplus/fragmentpractice_20210327/fragments/FirstFragment.kt
+++ app/src/main/java/com/nepplus/fragmentpractice_20210327/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.nepplus.fragmentpractice_20210327.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.nepplus.fragmentpractice_20210327.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(){
 
@@ -17,5 +19,14 @@ class FirstFragment : Fragment(){
 
         return inflater.inflate(R.layout.fragment_first, container, false)
 
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        logBtn.setOnClickListener {
+
+            Log.d("Fragment에서 실행", "테스트용 로그 출력")
+        }
     }
 }
